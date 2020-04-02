1baf34583779f13474970c3cfc0cc1940d216a31
unex <7575866+unex@users.noreply.github.com>
2020-04-02 17:49:24 -0600

    gpt2 shit fuckin pipenv wont lock wtfff

--- README.md
+++ README.md
@@ -6,12 +6,31 @@ Move `.env.example` to `.env` and put your shit in there
 
 `REDDIT_TOKEN` can be found as `reddit_session` under Cookies in your browsers dev tools while visiting https://reddit.com
 
+## Standard Install
+
 ```bash
 pipenv install
 pipenv run python main.py
 ```
 
-"Set-it-and-forget-it" example
+## Experimental GPT-2 Matching
+
+Borrowed from [gpt-2-output-dataset](https://github.com/openai/gpt-2-output-dataset/tree/master/detector)
+
+```https://storage.googleapis.com/gpt-2/detector-models/v1/detector-large.pt```
+
+or
+
+```wget https://storage.googleapis.com/gpt-2/detector-models/v1/detector-base.pt```
+
+or find another one idk let me know how it goes
+
+```bash
+pipenv install --dev
+pipenv run python main.py
+```
+
+## "Set-it-and-forget-it" example
 
 ```bash
 while true; git pull; do pipenv run python main.py; sleep 10; done
---
+++ gpt2.py
@@ -0,0 +1,36 @@
+from transformers import RobertaForSequenceClassification, RobertaTokenizer
+
+import fire
+import torch
+
+# https://github.com/openai/gpt-2-output-dataset/blob/master/detector/server.py
+
+checkpoint = 'detector-large.pt'
+
+class Roberta:
+    def __init__(self):
+        self.device = 'cpu'
+
+        data = torch.load(checkpoint, map_location=self.device)
+        model_name = 'roberta-large' if data['args']['large'] else 'roberta-base'
+        self.model = RobertaForSequenceClassification.from_pretrained(model_name)
+        self.tokenizer = RobertaTokenizer.from_pretrained(model_name)
+
+        self.model.load_state_dict(data['model_state_dict'])
+        self.model.eval()
+
+    def query(self, query):
+        tokens = self.tokenizer.encode(query)
+        all_tokens = len(tokens)
+        tokens = tokens[:self.tokenizer.max_len - 2]
+        used_tokens = len(tokens)
+        tokens = torch.tensor([self.tokenizer.bos_token_id] + tokens + [self.tokenizer.eos_token_id]).unsqueeze(0)
+        mask = torch.ones_like(tokens)
+
+        with torch.no_grad():
+            logits = self.model(tokens.to(self.device), attention_mask=mask.to(self.device))[0]
+            probs = logits.softmax(dim=-1)
+
+        fake, real = probs.detach().cpu().flatten().numpy().tolist()
+
+        return fake, real
